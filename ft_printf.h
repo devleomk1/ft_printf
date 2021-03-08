86b94836536011ed9b4ac856479c266b44f72ab4
devleom1 <wltnskjs@naver.com>
2021-03-08 16:30:15 +0900

    add ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/08 14:43:30 by jisokang          #+#    #+#             */
+/*   Updated: 2021/03/08 16:23:56 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <string.h>
+# include "./libft/libft.h"
+
+#endif
