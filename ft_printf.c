c92c37667290f141359d05643621d17274b25577
devleom1 <wltnskjs@naver.com>
2021-03-08 17:17:08 +0900

    add ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/08 14:43:30 by jisokang          #+#    #+#             */
-/*   Updated: 2021/03/08 17:16:01 by jisokang         ###   ########.fr       */
+/*   Created: 2021/03/08 16:37:49 by jisokang          #+#    #+#             */
+/*   Updated: 2021/03/08 17:16:17 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-# include <unistd.h>
-# include <stdarg.h>
-# include <stdlib.h>
-# include <string.h>
-# include "./libft/libft.h"
-
-int	ft_printf(const char *s, ...);
-
-#endif
+int	ft_printf(const char *s, ...)
+{
+	return (0);
+}
